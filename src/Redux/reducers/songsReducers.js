c5cb7af1f4cb3b93ec4d0144a56ca94811262924
Dinakhaled <dina.elghndour@gmail.com>
2019-08-23 15:14:13 +0200

    add some comments

--- src/Redux/reducers/songsReducers.js
+++ src/Redux/reducers/songsReducers.js
@@ -10,6 +10,18 @@ import {
 export default (state = {}, action) => {
      switch (action.type) {
         case FETCH_SONGS: 
+            /*  collect object keys and make object for everyone
+                colors = [{hue: green}, {hue: blue}]
+                _.mapkeys(colors, 'hue') ==> 
+                    {
+                        "green": {
+                            "hue": green
+                        },
+                        "blue": {
+                            "hue": blue
+                        }
+                    }
+            */
             return { ...state, ..._.mapKeys(action.payload, 'id') };
         case FETCH_SONG: 
             return { ...state, [action.payload.id]: action.payload };
@@ -18,6 +30,7 @@ export default (state = {}, action) => {
         case EDIT_SONG: 
             return { ...state, [action.payload.id]: action.payload };
         case DELETE_SONG :
+            // to remove element using in delete requests 
             return _.omit(state, action.payload);
         default: 
             return state;
